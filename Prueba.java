f4e1052047fc145cfd0bdb9dedfd81a2dea52ebe
miguel <miguel.dedompablomusulen@gmail.com>
2022-10-07 12:10:05 +0200

    musica

--- Prueba.java
+++ Prueba.java
@@ -2,9 +2,9 @@ public class Prueba {
 
     public static void main(String args[]) {
 
-        System.out.println("Apellidos: de Dompablo Musulen");
         System.out.println("Edad: 18 años , Año de nacimiento: 2004");
         System.out.println("Comida favorita: Salmorejo");
+        System.out.println("Musica: Neffex");
 
     }
     
