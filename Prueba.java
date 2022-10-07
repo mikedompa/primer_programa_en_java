b98203c33c1d26961210d9b16b6fdae191851684
miguel <miguel.dedompablomusulen@gmail.com>
2022-10-07 10:50:09 +0200

    Comida

--- Prueba.java
+++ Prueba.java
@@ -4,6 +4,7 @@ public class Prueba {
 
         System.out.println("Apellidos: de Dompablo Musulen");
         System.out.println("Edad: 18 años , Año de nacimiento: 2004");
+        System.out.println("Comida favorita: Salmorejo");
 
     }
     
