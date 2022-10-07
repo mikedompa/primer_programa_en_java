30f9291f5bf1ac4a2a1ee95f981635502f3786ea
miguel <miguel.dedompablomusulen@gmail.com>
2022-10-07 10:48:28 +0200

    Edad

--- Prueba.java
+++ Prueba.java
@@ -3,6 +3,7 @@ public class Prueba {
     public static void main(String args[]) {
 
         System.out.println("Apellidos: de Dompablo Musulen");
+        System.out.println("Edad: 18 años , Año de nacimiento: 2004");
 
     }
     
